cd94ad7e5d5ad13a398e58c0a707e51fbaa562b0
micziz <miczicontent@gmail.com>
2023-05-13 18:37:16 +0200

    Add prettier and eslint and all other stuff

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,7 +15,8 @@ module.exports = {
         "sourceType": "module"
     },
     "plugins": [
-        "@typescript-eslint"
+        "@typescript-eslint",
+        "prettier"
     ],
     "rules": {
     }
